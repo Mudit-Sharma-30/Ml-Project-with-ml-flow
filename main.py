cb500bf2c8208a1f6a8474ec3bd848772a62dffe
Mudit Sharma <99650506+Mudit-Sharma-30@users.noreply.github.com>
2023-11-26 09:10:59 +0530

    Data Transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.Machine_learning_with_mlflow import logger
 from Machine_learning_with_mlflow.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Machine_learning_with_mlflow.pipelines.stage_02_data_validation import DataValidationTrainingPipeline
+from Machine_learning_with_mlflow.pipelines.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 logger.info("Welcome to our custom Logging")
 
@@ -19,6 +20,16 @@ STAGE_NAME = "Data Validation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/Machine_learning_with_mlflow/config/configuration.py
+++ src/Machine_learning_with_mlflow/config/configuration.py
@@ -1,6 +1,6 @@
 from src.Machine_learning_with_mlflow.constants import *
 from src.Machine_learning_with_mlflow.utils.common import read_yaml, create_directories
-from Machine_learning_with_mlflow.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from Machine_learning_with_mlflow.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -45,3 +45,15 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
