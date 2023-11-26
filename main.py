1f4319c70a71f2f42e1d790760f32b3b543dfabb
Mudit Sharma <99650506+Mudit-Sharma-30@users.noreply.github.com>
2023-11-26 08:54:21 +0530

    Data Validation added

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from src.Machine_learning_with_mlflow import logger
 from Machine_learning_with_mlflow.pipelines.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from Machine_learning_with_mlflow.pipelines.stage_02_data_validation import DataValidationTrainingPipeline
 
 logger.info("Welcome to our custom Logging")
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
